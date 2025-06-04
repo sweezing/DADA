10881bbafd051aad320340cc1b62065d2508c3ef
TestOITIB-2 <TestOITIB-2@zik.loc>
2025-06-04 09:31:27 +0500

    end

--- django_itself/app_origin/urls.py
+++ django_itself/app_origin/urls.py
@@ -1,9 +1,16 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.urls import path
 from . import views
 
+# Create a staff required decorator
+staff_required = user_passes_test(lambda u: u.is_staff, login_url='/admin/login')
+
 urlpatterns = [
-    path('', views.NewsListView.as_view(), name='news_list'),
-    path('new/', views.NewsCreateView.as_view(), name='news_create'),
-    path('<int:pk>/edit/', views.NewsUpdateView.as_view(), name='news_update'),
-    path('<int:pk>/delete/', views.NewsDeleteView.as_view(), name='news_delete'),
+    path('', views.NewsListViewForAll.as_view(), name='news_list_for_all'),
+    
+    # Wrap admin views with staff_required decorator
+    path('list/', staff_required(views.NewsListView.as_view()), name='news_list'),
+    path('new/', staff_required(views.NewsCreateView.as_view()), name='news_create'),
+    path('<int:pk>/edit/', staff_required(views.NewsUpdateView.as_view()), name='news_update'),
+    path('<int:pk>/delete/', staff_required(views.NewsDeleteView.as_view()), name='news_delete'),
 ]
--- django_itself/app_origin/views.py
+++ django_itself/app_origin/views.py
@@ -1,36 +1,49 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin  # Add this
 
 from .models import News
 from .forms import NewsForm
 
-### MAIN CLASSES ### 
+class AdminRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+    """Mixin that requires user to be logged in and have staff status"""
+    login_url = '/admin/login'
+    
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+        return redirect(self.login_url + '?next=' + self.request.path)
 
-class NewsListView(ListView):
+
+# Public view (no restriction)
+
+class NewsListViewForAll(ListView):
+    model = News 
+    template_name = 'magazine/news_list_for_all.html'
+    context_object_name = 'news'
+
+# Only admin
+
+class NewsListView(AdminRequiredMixin, ListView):
     model = News 
     template_name = 'magazine/news_list.html'
     context_object_name = 'news'
 
-class NewsCreateView(CreateView):
+class NewsCreateView(AdminRequiredMixin, CreateView):
     model = News
     form_class = NewsForm
     template_name = 'magazine/news_form.html'
     success_url = reverse_lazy('news_list')
 
-class NewsUpdateView(UpdateView):
+class NewsUpdateView(AdminRequiredMixin, UpdateView):
     model = News
     form_class = NewsForm
     template_name = 'magazine/news_form.html'
     success_url = reverse_lazy('news_list')
 
-class NewsDeleteView(DeleteView):
+class NewsDeleteView(AdminRequiredMixin, DeleteView):
     model = News
     template_name = 'magazine/news_confirm_delete.html'
     success_url = reverse_lazy('news_list')
-    
-    # def delete_image(self, request, *args, **kwargs):
-    #     self.news = self.get_object()
-    #     if self.object.image:
-    #         self.object.image.delete(save=False)
-    #     return super().delete(request, *args, **kwargs)
