2fbebce81296044185cbd82e2008d1c0a28d07a3
TestOITIB-2 <TestOITIB-2@zik.loc>
2025-06-12 10:40:11 +0500

    text editor added

--- django_itself/app_origin/forms.py
+++ django_itself/app_origin/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+from ckeditor.widgets import CKEditorWidget
 from .models import News
 
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
         fields = ['name', 'description', 'main_text', 'image']
+        widgets = {
+            'main_text': CKEditorWidget(),
+            # Optionally, uncomment to use CKEditor for description as well
+            # 'description': CKEditorWidget(config_name='default'),
+        }
--- django_itself/django_itself/settings.py
+++ django_itself/django_itself/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_origin',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CKEditor configuration
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': 'auto',
+    },
+}
